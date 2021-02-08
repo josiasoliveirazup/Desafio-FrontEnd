37ae97f7c0b40f041b949268650922ac24ef35ab
josiasoliveirazup <josias.oliveira@zup.com.br>
2021-02-08 14:46:15 -0300

    new

--- pagina2/js/control.js
+++
@@ -1,7 +0,0 @@
-function aparecerTexto() {
-    let el = document.getElementsByClassName("button") .innerHTML="Texto";
-  }
-
-  function reset() {
-    let el = document.querySelector("button").innerHTML = "";
-  }
