5fcf01e71438195a52ed0394d207d8adbac07d38
josiasoliveirazup <josias.oliveira@zup.com.br>
2021-04-19 16:39:09 -0300

    input

--- js/script.js
+++ js/script.js
@@ -74,4 +74,67 @@ $("#removediv4").click(function(){
     $('#user4').empty("");
 });
 
+var list = [{name: 'Marcelo ',imgUrl:"",email: 'marcelo.beck22@exemple.com',phone:'(960)-861-1890' , location: 'Uberlandia-MG' },
+            {name: 'Lorena ',email: 'lorena.beck22@exemple.com', phone:'(960)-861-1890' , location: 'São Paulo-SP' },
+            {name: 'Thais ',email: 'thais.beck22@exemple.com', phone:'(960)-861-1890' , location: 'Rio de Janeiro-RJ'},
+            {name: 'Maria ',email: 'maria.beck22@exemple.com', phone:'(960)-861-1890' , location: 'Belo Horizonte-MG' },
+            {name: 'Maria ',email: 'maria.beck22@exemple.com', phone:'(960)-861-1890' , location: 'Belo Horizonte-MG' }
+    ]
 
+  function render(employeeList){
+      var tableElement = document.getElementById("employees");
+    
+
+      employeeList.forEach(item=>{
+          console.log(item.name)
+          var row = document.createElement("tr");
+          var tdName = document.createElement("td");
+          var tdEmail = document.createElement("td");
+          var tdPhone = document.createElement("td");
+          var tdLocation = document.createElement("td");
+          var imgUrl = document.createElement("img")
+          imgUrl.src = 'https://picsum.photos/200 ';
+          imgUrl.classList.add("image");
+
+          
+          
+
+          tdName.innerHTML=item.name;
+          tdEmail.innerHTML=item.email;
+          tdPhone.innerHTML=item.phone;
+          tdLocation.innerHTML=item.location;
+          imgUrl.innerHTML=item.imgUrl
+          
+    
+
+          row.appendChild(imgUrl)  
+          row.appendChild(tdName)
+          row.appendChild(tdEmail)
+          row.appendChild(tdPhone)
+          row.appendChild(tdLocation)
+         
+        
+    
+
+        tableElement.appendChild(row)
+          
+      })
+  }     
+
+  render(list)
+
+    function filter(filter){
+        console.log('Filter' , filter)
+        if (filter ==='all')
+        render(list)
+
+
+        if (filter ==='deleted')
+        render(list.filter(client =>client.deleted))
+
+
+        if (filter ==='finished')
+        render(list.filter(client => cliente.finished))
+
+    }
+  
